cf0b7047dad75823e033ad1fa9b8497857ad053e
João Manoel <jmaoliveira.cic@uesc.br>
2024-09-21 18:44:21 -0300

    Add API endpoints for department, course, professor, student, project, and skill models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,56 @@
 from django.db import models
 
-# Create your models here.
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    department_id = models.ForeignKey(Department, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Professor(models.Model):
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
+class Skill(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+
+
+class Student(models.Model):
+    registration = models.CharField(max_length=100, unique=True)
+    name = models.CharField(max_length=100)
+    email = models.EmailField()
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    bio = models.TextField()
+    skills = models.ManyToManyField(Skill)
+
+    def __str__(self):
+        return self.name
+
+
+class Project(models.Model):
+    title = models.CharField(max_length=100)
+    description = models.TextField()
+    type = models.CharField(max_length=100)
+    start_date = models.DateField()
+    end_date = models.DateField()
+    skills = models.ManyToManyField(Skill)
+    professor_id = models.ForeignKey(Professor, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
---
+++ api/serializers.py
@@ -0,0 +1,62 @@
+from rest_framework import serializers
+from .models import (
+    Department,
+    Course,
+    Professor,
+    Student,
+    Project,
+    Skill,
+)
+
+
+class DepartmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Department
+        fields = ["id", "name"]
+
+
+class CourseSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Course
+        fields = ["id", "name", "department_id"]
+
+
+class ProfessorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Professor
+        fields = ["id", "name", "email", "department", "bio"]
+
+
+class SkillSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Skill
+        fields = ["id", "title", "description"]
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = [
+            "id",
+            "registration",
+            "name",
+            "email",
+            "course",
+            "bio",
+            "skills"
+        ]
+
+
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = [
+            "id",
+            "title",
+            "description",
+            "type",
+            "start_date",
+            "end_date",
+            "skills",
+            "professor_id",
+        ]
