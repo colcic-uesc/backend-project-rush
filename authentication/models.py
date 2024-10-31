b0a1e18577feaa6e87197d7fd3c23598b44a177a
João Manoel <jmaoliveira.cic@uesc.br>
2024-10-30 22:12:13 -0300

    Update User model to remove null and blank options for 'rules' field

--- authentication/models.py
+++ authentication/models.py
@@ -6,7 +6,7 @@ from django.contrib.auth.models import User as BaseUser
 # username, first_name, last_name, email, password, groups, user_permissions,
 # is_staff, is_active, is_superuser, last_login, date_joined
 class User(BaseUser):
-    rules = models.CharField(max_length=100, blank=True, null=True)
+    rules = models.CharField(max_length=100)
 
     def __str__(self):
         return self.username
