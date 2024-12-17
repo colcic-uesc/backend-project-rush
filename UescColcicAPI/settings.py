80f3abeb1cfad9fa9149e4f4b2f47cc809426bc0
João Manoel <jmaoliveira.cic@uesc.br>
2024-12-16 22:57:30 -0300

    Configurando o middleware de CORS pelo no settings.py

--- UescColcicAPI/settings.py
+++ UescColcicAPI/settings.py
@@ -47,13 +47,14 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework.authtoken',
+    'corsheaders',
     'rest_framework',
     'drf_spectacular',
-    'corsheaders',
     'api',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -61,7 +62,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
     'api.middleware.log.LogMiddleware',
     'api.middleware.app.AppMiddleware',
 ]
